a810152cc61dcea11a1481785a1f8a6dadce7e9a
HeroYu920 <52651756+HeroYu920@users.noreply.github.com>
2024-01-09 22:39:24 +0800

    Update diy-part1.sh

--- config/Lienol-master/diy-part1.sh
+++ config/Lienol-master/diy-part1.sh
@@ -12,3 +12,8 @@
 # other
 # rm -rf package/utils/{ucode,fbtest}
 
+#Modify default gateway and netmask address and add passwall src
+sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+sed -i 's/255.255.255.0/255.255.240.0/g' package/base-files/files/bin/config_generate
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
